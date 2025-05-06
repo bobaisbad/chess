6c3cdaf4165185833ae918e9f71d188445a398ec
bobaisbad <legomaster1007@gmail.com>
2025-05-05 22:42:55 -0600

    Implemented ChessPiece methods; Updated implementation of ChessBoard with a shift in index values to compensate for coordinate differences (squares: 0-7, ChessPosition: 1-8); Beginning construction of the PieceMoves Calculator and BishopMovesCalculator classes

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -1,4 +1,121 @@
 package chess;
 
+import java.util.Collection;
+import java.util.ArrayList;
+
 public class BishopMovesCalculator {
+//    private ChessPiece piece;
+//    private ChessGame.TeamColor color;
+
+    public Collection<ChessMove> bishopCalculator(ChessPiece piece, ChessGame.TeamColor color, ChessPosition myPosition, ChessBoard board) {
+//        System.out.println("Creating vars...");
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        ArrayList<ChessMove> movesArray = new ArrayList<>();
+        ChessPosition newPosition = new ChessPosition(row, col);
+        row += 1;
+        col += 1;
+        newPosition.setRow(row);
+        newPosition.setCol(col);
+//        System.out.println("Starting to search...");
+
+        while (row <= 8 && col <= 8) { // && (board.getPiece(newPosition) == null || board.getPiece(newPosition).getTeamColor() != color)) {
+//            System.out.println("Piece type: " + board.getPiece(newPosition));
+            if (board.getPiece(newPosition) == null) {
+                System.out.println("Endpoint: {" + row + ", " + col + "}");
+                movesArray.add(new ChessMove(myPosition, new ChessPosition(row, col), null));
+            } else if (board.getPiece(newPosition).getTeamColor() != color) {
+//                System.out.println("Type: " + board.getPiece(newPosition).getPieceType());
+                System.out.println("Endpoint: {" + row + ", " + col + "}");
+                movesArray.add(new ChessMove(myPosition, new ChessPosition(row, col), null));
+                break;
+            } else {
+                break;
+            }
+
+            row += 1;
+            col += 1;
+            newPosition.setRow(row);
+            newPosition.setCol(col);
+        }
+
+        row = myPosition.getRow() - 1;
+        col = myPosition.getColumn() + 1;
+        newPosition.setRow(row);
+        newPosition.setCol(col);
+
+        while (row >= 1 && col <= 8) { // && (board.getPiece(newPosition) == null || board.getPiece(newPosition).getTeamColor() != color)) {
+//            System.out.println("Piece type: " + board.getPiece(newPosition));
+            if (board.getPiece(newPosition) == null) {
+                System.out.println("Endpoint: {" + row + ", " + col + "}");
+                movesArray.add(new ChessMove(myPosition, new ChessPosition(row, col), null));
+            } else if (board.getPiece(newPosition).getTeamColor() != color) {
+//                System.out.println("Type: " + board.getPiece(newPosition).getPieceType());
+                System.out.println("Endpoint: {" + row + ", " + col + "}");
+                movesArray.add(new ChessMove(myPosition, new ChessPosition(row, col), null));
+                break;
+            } else {
+                break;
+            }
+
+            row -= 1;
+            col += 1;
+            newPosition.setRow(row);
+            newPosition.setCol(col);
+        }
+
+        row = myPosition.getRow() - 1;
+        col = myPosition.getColumn() - 1;
+        newPosition.setRow(row);
+        newPosition.setCol(col);
+
+        while (row >= 1 && col >= 1) { // && (board.getPiece(newPosition) == null || board.getPiece(newPosition).getTeamColor() != color)) {
+//            System.out.println("Piece type: " + board.getPiece(newPosition));
+            if (board.getPiece(newPosition) == null) {
+                System.out.println("Endpoint: {" + row + ", " + col + "}");
+                movesArray.add(new ChessMove(myPosition, new ChessPosition(row, col), null));
+            } else if (board.getPiece(newPosition).getTeamColor() != color) {
+//                System.out.println("Type: " + board.getPiece(newPosition).getPieceType());
+                System.out.println("Endpoint: {" + row + ", " + col + "}");
+                movesArray.add(new ChessMove(myPosition, new ChessPosition(row, col), null));
+                break;
+            } else {
+                break;
+            }
+
+            row -= 1;
+            col -= 1;
+            newPosition.setRow(row);
+            newPosition.setCol(col);
+        }
+
+        row = myPosition.getRow() + 1;
+        col = myPosition.getColumn() - 1;
+        newPosition.setRow(row);
+        newPosition.setCol(col);
+
+        while (row <= 8 && col >= 1) { // && (board.getPiece(newPosition) == null || board.getPiece(newPosition).getTeamColor() != color)) {
+//            System.out.println("Piece type: " + board.getPiece(newPosition));
+            if (board.getPiece(newPosition) == null) {
+                System.out.println("Endpoint: {" + row + ", " + col + "}");
+                movesArray.add(new ChessMove(myPosition, new ChessPosition(row, col), null));
+            } else if (board.getPiece(newPosition).getTeamColor() != color) {
+//                System.out.println("Type: " + board.getPiece(newPosition).getPieceType());
+                System.out.println("Endpoint: {" + row + ", " + col + "}");
+                movesArray.add(new ChessMove(myPosition, new ChessPosition(row, col), null));
+                break;
+            } else {
+                break;
+            }
+
+            row += 1;
+            col -= 1;
+            newPosition.setRow(row);
+            newPosition.setCol(col);
+        }
+
+        return movesArray;
+    }
+
+
 }
--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,4 +1,31 @@
 package chess;
 
+import java.util.Collection;
+
 public class PieceMovesCalculator {
+    private ChessPiece piece;
+    private ChessPiece.PieceType type;
+    private ChessGame.TeamColor color;
+
+    public Collection<ChessMove> calculator(ChessBoard board, ChessPosition myPosition) {
+        this.piece = board.getPiece(myPosition);
+        this.type = piece.getPieceType();
+        this.color = piece.getTeamColor();
+
+//        if (type == ChessPiece.PieceType.BISHOP) {
+        BishopMovesCalculator calc = new BishopMovesCalculator();
+//        System.out.println("Pushing into Bishop Calc...");
+        return calc.bishopCalculator(piece, color, myPosition, board);
+//        } else if (type == ChessPiece.PieceType.QUEEN) {
+//            //
+//        } else if (type == ChessPiece.PieceType.ROOK) {
+//            //
+//        } else if (type == ChessPiece.PieceType.KNIGHT) {
+//            //
+//        } else if (type == ChessPiece.PieceType.BISHOP) {
+//            return BishopMovesCalculator();
+//        } else {
+//            //
+//        }
+    }
 }
